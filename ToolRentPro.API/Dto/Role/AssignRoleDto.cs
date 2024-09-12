8548224ce32439dec6004a8fce223561961518c3
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-12 16:48:53 -0300

    Create assign role dto

---
+++ ToolRentPro.API/Dto/Role/AssignRoleDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ToolRentPro.API.Dto.Role;
+
+public class AssignRoleDto
+{
+    public string UserId { get; set; } = string.Empty;
+    public string RoleId { get; set; } = string.Empty;
+}

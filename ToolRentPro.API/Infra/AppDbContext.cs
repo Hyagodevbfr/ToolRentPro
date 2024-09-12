080541832dc4b4ff7a8a91a124e399434fda2de1
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-12 07:55:17 -0300

    Add sample configuration for appsettings

---
+++ ToolRentPro.API/Infra/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ToolRentPro.API.Model;
+
+namespace ToolRentPro.API.Infra;
+
+public class AppDbContext : IdentityDbContext<UserModel>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+}

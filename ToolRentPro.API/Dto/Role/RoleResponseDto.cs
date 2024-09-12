0f5cdaa198f50ad3003046fc193919907d85eabf
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-12 16:20:38 -0300

    Create RoleResponse Dto

---
+++ ToolRentPro.API/Dto/Role/RoleResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ToolRentPro.API.Dto.Role;
+
+public class RoleResponseDto
+{
+    public string? Id { get; set; }
+    public string? Name { get; set; }
+    public int TotalUsers { get; set; }
+}

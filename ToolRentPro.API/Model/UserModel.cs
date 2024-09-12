b28439f330d54e2080a8cec6b07fcb29db0736c4
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-12 05:21:25 -0300

    Create User Model

---
+++ ToolRentPro.API/Model/UserModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ToolRentPro.API.Model;
+
+public class UserModel : IdentityUser
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}

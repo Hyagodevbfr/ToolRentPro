6fa41fd3b3bc2a5e8e9b88517607d18fcfeddccf
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-12 10:20:04 -0300

    Configure Jwt in program

--- ToolRentPro.API/Program.cs
+++ ToolRentPro.API/Program.cs
@@ -1,17 +1,72 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 using ToolRentPro.API.Infra;
+using ToolRentPro.API.Model;
 
 var builder = WebApplication.CreateBuilder(args);
+var jwtSettings = builder.Configuration.GetSection("JwtSetting");
 
 // Add services to the container.
 string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection")!;
 
 builder.Services.AddDbContextPool<AppDbContext>(options => options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
 
+builder.Services.AddIdentity<UserModel,IdentityRole>( ).AddEntityFrameworkStores<AppDbContext>( ).AddDefaultTokenProviders( );
+
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidAudience = jwtSettings["ValidAudience"],
+        ValidIssuer = jwtSettings["ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.GetSection("securityKey").Value!))
+    };
+});
+
 builder.Services.AddControllers( );
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer( );
-builder.Services.AddSwaggerGen( );
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer",new OpenApiSecurityScheme
+    {
+        Description = @"Jwt authorization example: 'Bearer eyJdhslfofpdfmvhfd.'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement( ){
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer",
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 var app = builder.Build( );
 
@@ -24,6 +79,7 @@ if(app.Environment.IsDevelopment( ))
 
 app.UseHttpsRedirection( );
 
+app.UseAuthentication();
 app.UseAuthorization( );
 
 app.MapControllers( );

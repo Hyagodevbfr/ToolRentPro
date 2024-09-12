4e93972f19d72346a1430b05ad334a3da9339ce1
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-12 15:54:57 -0300

    Create RoleCreate endpoint

---
+++ ToolRentPro.API/Controllers/RoleController/RoleController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ToolRentPro.API.Dto.Role;
+using ToolRentPro.API.Model;
+
+namespace ToolRentPro.API.Controllers.RoleController;
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class RoleController: ControllerBase
+{
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly UserManager<UserModel> _userManager;
+
+    public RoleController(RoleManager<IdentityRole> roleManager, UserManager<UserModel> userManager)
+    {
+        _roleManager = roleManager;
+        _userManager = userManager;
+    }
+
+    [HttpPost("/create")]
+    public async Task<IActionResult> CreateRole([FromBody] RoleCreateDto roleCreateDto)
+    {
+        if(roleCreateDto.RoleName is null)
+            return BadRequest("O nome da função é obrigatório");
+
+        var roleExist = await _roleManager.RoleExistsAsync(roleCreateDto.RoleName);
+        if(roleExist)
+            return BadRequest("Função já criada.");
+
+        var result = await _roleManager.CreateAsync(new IdentityRole { Name = roleCreateDto.RoleName });
+        if(result.Succeeded)
+            return Ok(new {message = "Função criada com sucesso."});
+
+        return BadRequest("Falha ao criar nova função.");
+    }
+}

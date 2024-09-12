46a31fd4ae12cbdfa771c105cb3eed25936909ab
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-12 12:54:42 -0300

    Create UserDetail Dto

---
+++ ToolRentPro.API/Dto/User/UserDetailDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ToolRentPro.API.Dto.User;
+
+public class UserDetailDto
+{
+    public string? Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string[]? Roles { get; set; }
+    public string? PhoneNumber { get; set; }
+    public bool TwoFactorEnabled { get; set; }
+    public bool PhoneNumberConfirmed { get; set; }
+    public int AcessFailedCount { get; set; }
+}

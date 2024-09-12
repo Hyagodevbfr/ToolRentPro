cd0ca75bec1dff07efa7df35c3ccb3cc07b3fb66
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-12 12:22:46 -0300

    Add AutoMapper

---
+++ ToolRentPro.API/Profiles/ProfileAutoMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using ToolRentPro.API.Dto.User;
+using ToolRentPro.API.Model;
+
+namespace ToolRentPro.API.Profiles;
+
+public class ProfileAutoMapper : Profile
+{
+    public ProfileAutoMapper()
+    {
+        CreateMap<UserRegisterDto,UserModel>( );
+    }
+}

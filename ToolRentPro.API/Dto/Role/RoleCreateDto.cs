d4ad3a7aed9501681e455cf05c956973d67f79a0
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-12 15:33:53 -0300

    Create RoleCreate dto

---
+++ ToolRentPro.API/Dto/Role/RoleCreateDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToolRentPro.API.Dto.Role;
+
+public class RoleCreateDto
+{
+    [Required(ErrorMessage = "O nome da função é obrigatório.")]
+    public string RoleName { get; set; } = string.Empty;
+}

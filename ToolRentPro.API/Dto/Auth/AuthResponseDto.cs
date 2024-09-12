8098e8d0ba8801839c4e9459b6e0fc9c3f850872
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-12 11:24:37 -0300

    Create AuthResponse Dto

---
+++ ToolRentPro.API/Dto/Auth/AuthResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ToolRentPro.API.Dto.Auth;
+
+public class AuthResponseDto
+{
+    public string? Token { get; set; }
+    public string? Message { get; set; }
+    public bool IsSuccess { get; set; }
+}

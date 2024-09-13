815fb46f884d735789f5b53b68c29ee1b22384cf
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-13 10:20:13 -0300

    Create maintenance entity

---
+++ ToolRentPro.API/Model/Maintenances/Maintenance.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ToolRentPro.API.Enums.Maintenance;
+using ToolRentPro.API.Model.Tool;
+
+namespace ToolRentPro.API.Model.Maintenances;
+
+public class Maintenance
+{
+    [Key]
+    public Guid Id { get; set; }
+    public ToolModel? Tool { get; set; }
+    public Guid? ToolId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string? ProblemDescription { get; set; }
+    public decimal MaintenanceCost { get; set; }
+    public MaintenanceEnum Status { get; set; }
+
+
+    protected Maintenance()
+    {
+        Id = Guid.NewGuid();
+    }
+}

e5dd359962227e62cba3c678104ec35bdae5f1aa
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-13 10:17:53 -0300

    Create Maintenance enum

---
+++ ToolRentPro.API/Enums/Maintenance/MaintenanceEnum.cs
@@ -0,0 +1,10 @@
+﻿using ToolRentPro.API.Model.Maintenances;
+
+namespace ToolRentPro.API.Enums.Maintenance;
+
+public enum MaintenanceEnum
+{
+    pending = 0,
+    UnderMaintenance = 1,
+    Completed = 2
+}

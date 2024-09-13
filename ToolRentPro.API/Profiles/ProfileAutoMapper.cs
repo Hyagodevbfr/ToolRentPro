3c40ffeb401b8819070efc34f71ad913351fe2fc
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-13 13:44:47 -0300

    Create Map CategoryDto and Category

--- ToolRentPro.API/Profiles/ProfileAutoMapper.cs
+++ ToolRentPro.API/Profiles/ProfileAutoMapper.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using ToolRentPro.API.Dto.Category;
 using ToolRentPro.API.Dto.User;
+using ToolRentPro.API.Model.Categories;
 using ToolRentPro.API.Model.User;
 
 namespace ToolRentPro.API.Profiles;
@@ -9,5 +11,11 @@ public class ProfileAutoMapper : Profile
     public ProfileAutoMapper()
     {
         CreateMap<UserRegisterDto,UserModel>( );
+
+        CreateMap<CategoryDto,Category>( )
+            .ForMember(dest => dest.CreatedBy,opt => opt.MapFrom((src,dest,_,context) => context.Items["UserId"]))
+            .ForMember(dest => dest.CreatedAt,opt => opt.MapFrom(_ => DateTime.UtcNow))
+            .ForMember(dest => dest.EditedBy,opt => opt.MapFrom((src,dest,_,context) => context.Items["UserId"]))
+            .ForMember(dest => dest.LastUpdate,opt => opt.MapFrom(_ => DateTime.UtcNow));
     }
 }

9d553d635f3d583f1bab19d07c35bc60dcfd2288
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-13 10:19:37 -0300

    Create category entity

---
+++ ToolRentPro.API/Model/Categories/Category.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToolRentPro.API.Model.Categories;
+
+public class Category : Entity
+{
+    [Required]
+    public string Name { get; set; } = null!;
+}

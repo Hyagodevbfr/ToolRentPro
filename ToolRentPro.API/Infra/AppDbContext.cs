4b7ac6c2c2fd86c1499c161e7e3dd4e84194ddc7
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-13 11:28:06 -0300

    Update AppDbContext

--- ToolRentPro.API/Infra/AppDbContext.cs
+++ ToolRentPro.API/Infra/AppDbContext.cs
@@ -1,13 +1,42 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+using ToolRentPro.API.Model.Categories;
+using ToolRentPro.API.Model.Maintenances;
+using ToolRentPro.API.Model.Tool;
 using ToolRentPro.API.Model.User;
 
 namespace ToolRentPro.API.Infra;
 
 public class AppDbContext : IdentityDbContext<UserModel>
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+
+    public DbSet<ToolModel>? Tools { get; set; }
+    public DbSet<Maintenance>? Maintenances { get; set; }
+    public DbSet<Category>? Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
     {
-        
+        base.OnModelCreating(builder);
+
+        builder.Entity<ToolModel>( )
+            .ToTable("Tools");
+
+        builder.Entity<Maintenance>( )
+            .ToTable("Maintenances");
+
+        builder.Entity<Category>( )
+            .ToTable("Categories");
+
+        builder.Entity<Maintenance>( )
+            .HasOne(m => m.Tool)
+            .WithMany(t => t.MaintenanceHistory)
+            .HasForeignKey(m => m.ToolId);
+
+        builder.Entity<ToolModel>( )
+            .HasOne(t => t.Category)
+            .WithMany( )
+            .HasForeignKey(t => t.CategoryId);
     }
 }

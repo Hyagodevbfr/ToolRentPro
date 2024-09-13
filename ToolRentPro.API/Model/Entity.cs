887e21fd9ddda5c34e682f586e8483b3c0765935
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-13 10:18:46 -0300

    Create entity base

---
+++ ToolRentPro.API/Model/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToolRentPro.API.Model;
+
+public abstract class Entity
+{
+    [Key] public Guid Id { get; set; }
+    public Guid CreatedBy { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public Guid EditedBy { get; set; }
+    public DateTime LastUpdate { get; set; } = DateTime.UtcNow;
+
+    protected Entity()
+    {
+        Id = Guid.NewGuid( );
+    }
+}

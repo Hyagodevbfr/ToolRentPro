4548ceb9843c2deac2c63968e663e078874ba4fd
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-14 07:44:02 -0300

    Update Mapper

--- ToolRentPro.API/Profiles/ProfileAutoMapper.cs
+++ ToolRentPro.API/Profiles/ProfileAutoMapper.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using ToolRentPro.API.Dto.Category;
+using ToolRentPro.API.Dto.Tool;
 using ToolRentPro.API.Dto.User;
 using ToolRentPro.API.Model.Categories;
+using ToolRentPro.API.Model.Tool;
 using ToolRentPro.API.Model.User;
 
 namespace ToolRentPro.API.Profiles;
@@ -13,6 +15,12 @@ public class ProfileAutoMapper : Profile
         CreateMap<UserRegisterDto,UserModel>( );
 
         CreateMap<CategoryDto,Category>( )
+            .ForMember(dest => dest.CreatedBy,opt => opt.MapFrom((src,dest,_,context) => context.Items["UserId"]))
+            .ForMember(dest => dest.CreatedAt,opt => opt.MapFrom(_ => DateTime.UtcNow))
+            .ForMember(dest => dest.EditedBy,opt => opt.MapFrom((src,dest,_,context) => context.Items["UserId"]))
+            .ForMember(dest => dest.LastUpdate,opt => opt.MapFrom(_ => DateTime.UtcNow));
+
+        CreateMap<ToolCreateDto,ToolModel>()
             .ForMember(dest => dest.CreatedBy,opt => opt.MapFrom((src,dest,_,context) => context.Items["UserId"]))
             .ForMember(dest => dest.CreatedAt,opt => opt.MapFrom(_ => DateTime.UtcNow))
             .ForMember(dest => dest.EditedBy,opt => opt.MapFrom((src,dest,_,context) => context.Items["UserId"]))

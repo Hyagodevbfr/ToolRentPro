128c6e4aee73ce8c694f6db6e32617f020b9ef4b
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-14 07:41:48 -0300

    Apply migrations aplly migrations

---
+++ ToolRentPro.API/Migrations/20240913222711_CreateTables.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToolRentPro.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Tools",
+                newName: "NameTool");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NameTool",
+                table: "Tools",
+                newName: "Name");
+        }
+    }
+}

21a55feb3182cb6622d5619ee3be0dd4f50134e7
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-14 12:27:39 -0300

    Create Tool Create Dto

---
+++ ToolRentPro.API/Dto/Tool/ToolCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ToolRentPro.API.Enums.Tool;
+
+namespace ToolRentPro.API.Dto.Tool;
+
+public class ToolCreateDto
+{
+    public Guid CategoryId { get; set; }
+    public string NameTool { get; set; } = null!;
+    public string Description { get; set; } = string.Empty;
+    public string Brand { get; set; } = string.Empty;
+    public int StockQuantity { get; set; }
+    public decimal ToolCost { get; set; }
+    public AvailabilityEnum Availability { get; set; }
+}

7d0a59e1989e7eb61f2d43e82be2b21a024599a4
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-16 04:42:58 -0300

    Create GetDetailTool method

--- ToolRentPro.API/Controllers/ToolController/ToolController.cs
+++ ToolRentPro.API/Controllers/ToolController/ToolController.cs
@@ -3,9 +3,13 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
 using System.Security.Claims;
+using ToolRentPro.API.Dto.Role;
 using ToolRentPro.API.Dto.Tool;
 using ToolRentPro.API.Infra;
+using ToolRentPro.API.Model.Categories;
 using ToolRentPro.API.Model.Tool;
 using ToolRentPro.API.Model.User;
 
@@ -29,7 +33,7 @@ public class ToolController: ControllerBase
 
     }
 
-    [HttpPost]
+    [HttpPost("/createtool")]
     [Consumes("multipart/form-data")]
     public async Task<ActionResult> CreateTool(IFormFile photo,[FromForm] ToolCreateDto toolCreateDto)
     {
@@ -53,6 +57,37 @@ public class ToolController: ControllerBase
         await _appDbContext.SaveChangesAsync();
 
         return Ok(new { Message = "Ferramenta criada com sucesso."});
+    }
+
+    [HttpGet("/tools")]
+    public async Task<ActionResult<IList<ToolResponseDto>>> GetTools(int page, int rows)
+    {
+        var tools = await _appDbContext.Tools!
+        .Select(t => new ToolResponseDto
+        {
+            NameTool = t.NameTool,
+            CategoryId = t.CategoryId,
+            Description = t.Description,
+            Photo = t.Photo,
+            Brand = t.Brand,
+            StockQuantity = t.StockQuantity,
+            ToolCost = t.ToolCost,
+            Availability = t.Availability,
+            NecessaryMaintenance = t.NecessaryMaintenance
+        }).Skip(page - 1 * rows).Take(rows)
+        .ToListAsync( );
+
+        return Ok(tools);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<ToolModel>> GetDetail(Guid id)
+    {
+        var tool = await _appDbContext.Tools!.FindAsync(id);
+        if(tool is null)
+            return NotFound("Ferramenta não encontrada ou apagada.");
+
+        return Ok(tool);
     }
 
     private async Task<string> GenerateNameImage(IFormFile photo)

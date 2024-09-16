9c247501a71ee07ff9c88fcb7b878b880c8654ed
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-16 04:44:41 -0300

    Create Tool Response Dto

---
+++ ToolRentPro.API/Dto/Tool/ToolResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ToolRentPro.API.Enums.Tool;
+using ToolRentPro.API.Model.Categories;
+
+namespace ToolRentPro.API.Dto.Tool
+{
+    public class ToolResponseDto
+    {
+        public string NameTool { get; set; } = null!;
+        public Guid CategoryId { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string Photo { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public int StockQuantity { get; set; }
+        public decimal ToolCost { get; set; }
+        public AvailabilityEnum Availability { get; set; }
+        public bool NecessaryMaintenance { get; set; }
+    }
+}

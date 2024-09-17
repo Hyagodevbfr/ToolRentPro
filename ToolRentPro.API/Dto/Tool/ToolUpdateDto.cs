64b8bebca9caea5d1afd7664d8cd4e7010977478
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-17 04:27:22 -0300

    Create Tool update dto

---
+++ ToolRentPro.API/Dto/Tool/ToolUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using ToolRentPro.API.Enums.Tool;
+
+namespace ToolRentPro.API.Dto.Tool;
+
+public class ToolUpdateDto
+{
+    public string NameTool { get; set; } = null!;
+    public string Description { get; set; } = string.Empty;
+    public Guid CategoryId { get; set; }
+    public string Brand { get; set; } = string.Empty;
+    public int StockQuantity { get; set; }
+    public decimal ToolCost { get; set; }
+    public AvailabilityEnum Availability { get; set; }
+    public bool NecessaryMaintenance { get; set; }
+}

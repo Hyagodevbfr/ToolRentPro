a5898d3559ee79be13f884824863e88e22d6d75a
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-17 04:40:32 -0300

    Create Rental Tool model

---
+++ ToolRentPro.API/Model/RentalTools/RentalTool.cs
@@ -0,0 +1,11 @@
+﻿namespace ToolRentPro.API.Model.RentalTools;
+
+public class RentalTool
+{
+    public Guid Id { get; set; }
+
+    protected RentalTool()
+    {
+        Id = Guid.NewGuid();
+    }
+}

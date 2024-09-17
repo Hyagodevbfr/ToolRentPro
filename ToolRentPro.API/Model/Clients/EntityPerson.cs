b26232a7548ea186894223e68d42f83d2c5025e1
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-17 04:35:06 -0300

    Create customer base entity

---
+++ ToolRentPro.API/Model/Clients/EntityPerson.cs
@@ -0,0 +1,22 @@
+﻿namespace ToolRentPro.API.Model.Clients;
+
+public class EntityPerson
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    public Guid ClienteId { get; set; }
+
+    public string CEP { get; set; } = string.Empty;
+    public string Addres { get; set; } = string.Empty;
+    public string Complement { get; set; } = string.Empty;
+    public string Neighborhood { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+
+
+    protected EntityPerson()
+    {
+        Id = Guid.NewGuid();
+    }
+}

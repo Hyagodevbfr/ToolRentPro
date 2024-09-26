4fe2218412d03474c5e75a972055f7ea1d83c090
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-26 07:24:14 -0300

    Update Individual Model

---
+++ ToolRentPro.API/Model/Clients/IndividualModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToolRentPro.API.Model.Clients;
+
+public class IndividualModel : EntityPerson
+{
+    [Required]
+    public string FirstName { get; set; } = null!;
+    [Required]
+    public string LastName { get; set; } = null!;
+    [Required]
+    public string CPF { get; set; } = null!;
+    [Required]
+    public string Rg { get; set; } = null!;
+}

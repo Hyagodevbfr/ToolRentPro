3c072ede0753a31eec771ee53adfe4dc3feffcdb
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-26 06:49:31 -0300

    Add Required Data Anotation

--- ToolRentPro.API/Model/Clients/EntityPerson.cs
+++ ToolRentPro.API/Model/Clients/EntityPerson.cs
@@ -1,22 +1,27 @@
-﻿namespace ToolRentPro.API.Model.Clients;
+﻿using System.ComponentModel.DataAnnotations;
+using ToolRentPro.API.Enums.Client;
 
-public class EntityPerson
+namespace ToolRentPro.API.Model.Clients;
+
+public class EntityPerson : Entity
 {
-    public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-
-    public Guid ClienteId { get; set; }
-
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    [Required]
+    public ICollection<PhoneModel> Phones { get; set; } = new List<PhoneModel>();
+    [Required] 
     public string CEP { get; set; } = string.Empty;
+    [Required] 
     public string Addres { get; set; } = string.Empty;
+    [Required] 
     public string Complement { get; set; } = string.Empty;
+    [Required] 
     public string Neighborhood { get; set; } = string.Empty;
+    [Required] 
     public string Location { get; set; } = string.Empty;
+    [Required] 
     public string State { get; set; } = string.Empty;
-
-
-    protected EntityPerson()
-    {
-        Id = Guid.NewGuid();
-    }
+    [Required] 
+    public FidelityEnum Fidelity { get; set; }
 }

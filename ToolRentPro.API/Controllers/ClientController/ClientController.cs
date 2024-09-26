758a8f77ca021d20f790664808bf8ac3e9b0b697
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-26 07:22:05 -0300

    Update Client Controller

---
+++ ToolRentPro.API/Controllers/ClientController/ClientController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ToolRentPro.API.Infra;
+using ToolRentPro.API.Model.User;
+
+namespace ToolRentPro.API.Controllers.ClientController;
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class ClientController: ControllerBase
+{
+    private readonly AppDbContext _appDbContext;
+    private readonly IMapper _mapper;
+    private readonly UserManager<UserModel> _userManager;
+    public ClientController(AppDbContext appDbContext,IMapper mapper,UserManager<UserModel> userManager)
+    {
+        _appDbContext = appDbContext;
+        _mapper = mapper;
+        _userManager = userManager;
+    }
+
+}

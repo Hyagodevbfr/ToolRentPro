771fb1dabafc84c0b8f7374e26590e60907d2993
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-26 07:20:06 -0300

    Create phone Model

--- ToolRentPro.API/Model/Clients/PhoneModel.cs
+++ ToolRentPro.API/Model/Clients/PhoneModel.cs
@@ -1,5 +1,14 @@
-﻿namespace ToolRentPro.API.Model.Clients;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToolRentPro.API.Model.Clients;
 
 public class PhoneModel
 {
+    public Guid Id { get; set; } = Guid.NewGuid( );
+    [Required]
+    public string Number { get; set; } = string.Empty;
+
+    public Guid ClientId { get; set; }
+    public EntityPerson Client { get; set; } = null!;
+
 }

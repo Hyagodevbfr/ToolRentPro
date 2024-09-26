10b8c3261ca66c157aa7365de9d795ee60784e00
hyagodevbfr <hyago.sbrito@gmail.com>
2024-09-26 07:23:46 -0300

    Update Business Model

---
+++ ToolRentPro.API/Model/Clients/BusinessModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToolRentPro.API.Model.Clients;
+
+public class BusinessModel : EntityPerson
+{
+    [Required]
+    public string CompanyName { get; set; } = null!;
+    [Required]
+    public string TradeName { get; set; } = null!;
+}
